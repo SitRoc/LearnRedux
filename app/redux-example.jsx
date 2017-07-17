1536331a44a81550bf5bb4aa3b1c836648ff52bb
Steve <stevecortis@live.com>
2017-07-17 11:44:43 +0200

    Add actions for adding/removing hobbies and movies

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -2,7 +2,16 @@ var redux = require('redux');
 
 console.log('Starting Redux Example');
 
-var reducer = (state = {name: 'Anonymous'}, action) => {
+var stateDefault = {
+  name: 'Anonymous',
+  hobbies: [],
+  movies: []
+};
+
+var nextHobbyId = 1;
+var nextMovieId = 1;
+
+var reducer = (state = stateDefault, action) => {
   //state = state || {name: 'Anonymous'};
   switch (action.type) {
     case 'CHANGE_NAME':
@@ -10,6 +19,39 @@ var reducer = (state = {name: 'Anonymous'}, action) => {
         ...state,
         name: action.name
       };
+    case 'ADD_HOBBY':
+      return {
+        ...state,
+        hobbies: [
+          ...state.hobbies,
+          {
+            id: nextHobbyId++,
+            hobby: action.hobby
+          }
+        ]
+      };
+    case 'REMOVE_HOBBY':
+        return {
+          ...state,
+          hobbies: state.hobbies.filter((hobby) => hobby.id !== action.id)
+        };
+    case 'ADD_MOVIE':
+        return {
+          ...state,
+          movies: [
+            ...state.movies,
+            {
+              id: nextMovieId++,
+              title: action.name,
+              genre: action.genre
+            }
+          ]
+        };
+    case 'REMOVE_MOVIE':
+        return {
+          ...state,
+          movies: state.movies.filter((movie) => movie.id !== action.id)
+        };
     default:
       return state;
   }
@@ -21,6 +63,7 @@ var unsubscribe = store.subscribe(() => {
   var state = store.getState();
 
   console.log('Name is', state.name);
+  console.log('New state', store.getState());
   document.getElementById('app').innerHTML = state.name;
 });
 
@@ -37,6 +80,40 @@ var action = {
 store.dispatch(action);
 
 store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'Running'
+});
+
+store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'Walking'
+});
+
+// Remove hobby
+store.dispatch({
+  type: 'REMOVE_HOBBY',
+  id: 2
+});
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  name: 'Going out in style',
+  genre: 'Comedy'
+});
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  title: 'Interstellar',
+  genre: 'Thriller'
+});
+
+// Remove movie
+store.dispatch({
+  type: 'REMOVE_MOVIE',
+  id: 2
+});
+
+store.dispatch({
   type: 'CHANGE_NAME',
-  name: 'Marie Mifsud'
+  title: 'Marie Mifsud'
 });
